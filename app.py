453f370c43b17015b1b96dabf07bb381e241ef2d
Gaurav Sharma <greatsharma04@gmail.com>
2019-12-21 12:36:13 +0530

    added exception handling

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
 import cv2
 from utils import getColorPalette
 from flask import Flask, request, jsonify
-
+from exceptions import URLException, CV2Exception
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def home():
-    if 'src' not in request.args:
-        return jsonify({'code': 404, 'error': "no source image found"})
-
-    src = request.args['src']
-    dominant_color, palette = getColorPalette(src)
 
     response = {
-        'dominantColor': str('hsv' + str(dominant_color)),
-        'palette': [str('hsv' + str(ele)) for ele in palette]
+        'status': {
+            'code': None,
+            'msg': ""
+        }
     }
+
+    if 'src' not in request.args:
+        response['status']['code'] = 404
+        response['status']['msg'] = "Specify source image"
+    else:
+        src = request.args['src']
+        try:
+            dominant_color, palette = getColorPalette(src)
+            response['status']['code'] = 200
+            response['status']['msg'] = "OK"
+            response['dominantColor'] = str('hsv' + str(dominant_color))
+            response['palette'] = [str('hsv' + str(ele)) for ele in palette]
+        except URLException:
+            response['status']['code'] = 404
+            response['status']['msg'] = "Image not found"
+        except CV2Exception:
+            response['status']['code'] = 500
+            response['status']['msg'] = "Internal server error"
 
     return jsonify(response)
 
